084fde09ac7e407da9e941a69a4cb8fcfeda2799
walchko <walchko@users.noreply.github.com>
2019-08-05 20:09:28 -0400

    up

---
+++ example/watcher/watcher.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/core.hpp>
+#include <iostream>
+#include <string>
+#include "../../src/message.hpp"
+#include <gecko/gecko.hpp>
+
+
+using namespace cv;
+using namespace std;
+
+int main(){
+    return 0;
+}
